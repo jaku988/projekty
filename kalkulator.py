5382be715f36d0357c87e379a83b9d838186c471
jaku988 <107874980+jaku988@users.noreply.github.com>
2022-09-02 14:25:44 +0200

    Update kalkulator.py

--- kalkulator.py
+++ kalkulator.py
@@ -1,89 +1,92 @@
 from tkinter import *
 
-def button_press(num):
+def press_button(num):
     global equation_text
+
     equation_text = equation_text + str(num)
     equation_label.set(equation_text)
 
 def equal():
     global equation_text
-    try:
-        x = str(eval(equation_text))
-        equation_label.set(str(eval(equation_text)))
 
+    try:
         equation_text = str(eval(equation_text))
+        equation_label.set(equation_text)
     except ZeroDivisionError:
-        equation_label.set("Imbecil")
+        equation_label.set("ZeroDivisionError")
+        equation_text = ""
     except SyntaxError:
         equation_label.set("Wrong Syntax")
+        equation_text = ""
 
 def clear():
     global equation_text
+
     equation_text = ""
     equation_label.set(equation_text)
 
 window = Tk()
 window.title("Kalkulator")
-window.config(bg='#DDDDDD')
+window.config(bg='#27025e')
 
 equation_text = ""
 equation_label = StringVar()
 
-label = Label(window, textvariable=equation_label, bg='#FFFFFF', fg='black', width=30, height=4)
-label.pack()
-
-frame = Frame(window)
+frame = Frame(window, bg='#27025e')
 frame.pack()
 
-button1 = Button(frame, width=5, height=5, text=1, command=lambda: button_press(1))
-button1.grid(row=0, column=0)
+label = Label(frame, textvariable=equation_label, width=17, height=2, bg='#FFFFFF', font=('Consolas', 25))
+label.grid(row=0, column=0, columnspan=4)
 
-button2 = Button(frame, width=5, height=5, text=2, command=lambda: button_press(2))
-button2.grid(row=0, column=1)
+button1 = Button(frame, text=1, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(1))
+button1.grid(row=1, column=0)
 
-button3 = Button(frame, width=5, height=5, text=3, command=lambda: button_press(3))
-button3.grid(row=0, column=2)
+button2 = Button(frame, text=2, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(2))
+button2.grid(row=1, column=1)
 
-button4 = Button(frame, width=5, height=5, text=4, command=lambda: button_press(4))
-button4.grid(row=1, column=0)
+button3 = Button(frame, text=3, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(3))
+button3.grid(row=1, column=2)
 
-button5 = Button(frame, width=5, height=5, text=5, command=lambda: button_press(5))
-button5.grid(row=1, column=1)
+button4 = Button(frame, text=4, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(4))
+button4.grid(row=2, column=0)
 
-button6 = Button(frame, width=5, height=5, text=6, command=lambda: button_press(6))
-button6.grid(row=1, column=2)
+button5 = Button(frame, text=5, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(5))
+button5.grid(row=2, column=1)
 
-button7 = Button(frame, width=5, height=5, text=7, command=lambda: button_press(7))
-button7.grid(row=2, column=0)
+button6 = Button(frame, text=6, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(6))
+button6.grid(row=2, column=2)
 
-button8 = Button(frame, width=5, height=5, text=8, command=lambda: button_press(8))
-button8.grid(row=2, column=1)
+button7 = Button(frame, text=7, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(7))
+button7.grid(row=3, column=0)
 
-button9 = Button(frame, width=5, height=5, text=9, command=lambda: button_press(9))
-button9.grid(row=2, column=2)
+button8 = Button(frame, text=8, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(8))
+button8.grid(row=3, column=1)
 
-button0 = Button(frame, width=5, height=5, text=0, command=lambda: button_press(0))
-button0.grid(row=3, column=0)
+button9 = Button(frame, text=9, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(9))
+button9.grid(row=3, column=2)
 
-plus = Button(frame, width=5, height=5, text='+', command=lambda: button_press('+'))
-plus.grid(row=3, column=1)
+button0 = Button(frame, text=0, font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button(0))
+button0.grid(row=4, column=0)
 
-minus = Button(frame, width=5, height=5, text='-', command=lambda: button_press('-'))
-minus.grid(row=3, column=2)
+decimal = Button(frame, text='.', font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button('.'))
+decimal.grid(row=4, column=1)
 
-equal = Button(frame, width=5, height=5, text='=', command=equal)
-equal.grid(row=3, column=3)
+equal = Button(frame, text='=', font='SelawikSemibold', width=8, height=4, bg='#999999', command=equal)
+equal.grid(row=4, column=2)
 
-decimal = Button(frame, width=5, height=5, text='.', command=lambda: button_press('.'))
-decimal.grid(row=0, column=3)
+plus = Button(frame, text='+', font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button('+'))
+plus.grid(row=1, column=3)
 
-multiply = Button(frame, width=5, height=5, text='*', command=lambda: button_press('*'))
-multiply.grid(row=1, column=3)
+minus = Button(frame, text='-', font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button('-'))
+minus.grid(row=2, column=3)
 
-divide = Button(frame, width=5, height=5, text='/', command=lambda: button_press('/'))
-divide.grid(row=2, column=3)
+multiply = Button(frame, text='*', font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button('*'))
+multiply.grid(row=3, column=3)
 
-clear = Button(frame, width=10, height=5, text="Clear", command=clear)
-clear.grid(row=4, column=1, columnspan=2)
+divide = Button(frame, text='/', font='SelawikSemibold', width=8, height=4, bg='#999999', command=lambda: press_button('/'))
+divide.grid(row=4, column=3)
+
+clear = Button(frame, text='Clear', font='SelawikSemibold', width=12, height=4, bg='#999999', command=clear)
+clear.grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
